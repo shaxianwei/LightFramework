b84310e2388ba0cc88431dd1f4f8a6c512ab66ce
xianrendzw <14068728@qq.com>
2013-07-26 18:16:11 +0800

    fixed CategoryTest.java

--- lightframework.data.mysql/src/test/java/lightframework/data/mysql/CategoryTest.java
+++ lightframework.data.mysql/src/test/java/lightframework/data/mysql/CategoryTest.java
@@ -1,6 +1,9 @@
 package lightframework.data.mysql;
 
 import java.util.*;
+import java.util.regex.Matcher;
+import org.hamcrest.Description;
+import org.junit.*;
 
 /**
  *
@@ -8,15 +11,38 @@ import java.util.*;
  */
 public class CategoryTest {
 
-    private final static String url = "jdbc:mysql://192.168.1.251/test?user=root&password=ddd";
+    private final static String url = "jdbc:mysql://localhost/test?user=root&password=ddd";
+    private CategoryDAO dao;
 
     public void run() {
         CategoryDAO category = new CategoryDAO(null);
         category.deleteAll();
         List<CategoryEntity> categories = category.select();
         for (int i = 0; i < 10; i++) {
-            CategoryEntity dto = new CategoryEntity(i,"test" + i, "test1" + i);
+            CategoryEntity dto = new CategoryEntity(i, "test" + i, "test1" + i);
             int id = category.insertWithId(dto);
         }
+    }
+
+    @Before
+    public void startup() {
+        this.dao = new CategoryDAO(null);
+    }
+
+    @After
+    public void tearDown() {
+        this.dao  = null;
+    }
+
+    @Test
+    public void add() {
+        CategoryEntity entity = new CategoryEntity(1, "test1", "test1");
+        int id = this.dao.insertWithId(entity);
+        Assert.assertThat(id,null);
+    }
+    
+    @Test
+    public void delete(){
+        
     }
 }
