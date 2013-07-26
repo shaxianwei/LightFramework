4073b1abfc8042a7e4585ddd0682cc818b4abfcf
xianrendzw <14068728@qq.com>
2013-07-26 18:16:54 +0800

    create project

---
+++ lightframework.data.oracle/src/main/java/lightframework/data/oracle/App.java
@@ -0,0 +1,13 @@
+package lightframework.data.oracle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
